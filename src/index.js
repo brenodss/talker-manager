eeb51ac3de943eba2289d935d3f41b6575f3f3ce
brenodss <brenondss@gmail.com>
2022-08-28 15:54:58 -0300

    Estrutura inicial montada

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const checkTalkers = require('./middleWares/checkTalkers');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const fs = require('fs');
+// const { join } = require('path');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  // const completePath = join('/src');
+  const content = fs.readFileSync('./src/talker.json', 'utf-8');
+  const parseContent = JSON.parse(content);
+  res.status(200).send(content);
 });
 
 app.listen(PORT, () => {
---
+++ src/middleWares/checkTalkers.js
@@ -0,0 +1,8 @@
+/* const fs = require('fs');
+const { join } = require('path');
+
+const checkTalkers = (req, res, next) => {
+  next();
+};
+
+module.exports = checkTalkers; */
